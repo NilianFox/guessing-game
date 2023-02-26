0dc0c61eb59d6fe60055e834f76fadd08d1fb903
Danil Garmashov <wizzy_fox@mail.ru>
2023-02-26 21:26:54 +0700

    feat: completed quessing game js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num = Math.round((this.max + this.min) / 2);
+        return this.num;
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
+        this.min = this.num;
     }
 }
 
